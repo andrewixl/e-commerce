8d37a3dd3f7d96cc416eef7e0b4fdcf9b1435cb3
Andrew Burger <burger.andrewixl@gmail.com>
2017-07-27 12:02:24 -0700

    Add Models Changes

--- ecommerce/apps/login_app/models.py
+++ ecommerce/apps/login_app/models.py
@@ -5,17 +5,18 @@ import re
 import bcrypt
 
 # Create your models here.
-
-
 class UserManager(models.Manager):
 	def registerVal(self, postData):
 		results = {'status': True, 'errors': [], 'user': None}
-		if len(postData['first_name']) < 3:
+		if len(postData['first_name']) < 3 and len(postData['first_name']) > 50:
 			results['status'] = False
 			results['errors'].append('First Name Must be at Least 3 Characters.')
-		if len(postData['last_name']) < 3:
+		if len(postData['last_name']) < 3 and len(postData['last_name']) > 50:
 			results['status'] = False
 			results['errors'].append('Last Name Must be at Least 3 Characters.')
+		if len(postData['username']) < 3  and len(postData['first_name']) > 20:
+			results['status'] = False
+			results['errors'].append('Username Must be at Least 3 Characters.')
 		if not re.match(r"[^@]+@[^@]+\.[^@]+", postData['email']):
 			results['status'] = False
 			results['errors'].append('Please Enter a Valid Email.')
@@ -23,40 +24,42 @@ class UserManager(models.Manager):
 			results['status'] = False
 			results['errors'].append('Please Enter a Set of Matching Valid Password.')
 
-		user = User.objects.filter(email=postData['email'])
+		user = User.objects.filter(username=postData['username'])
 		print user, '*****', len(user)
 		if len(user) >= 1:
 			results['status'] = False
 			results['errors'].append(
-			    'User Already Exists, Please Login or Use New Email.')
+			    'User Already Exists, Please Login or Use a Different Username.')
 		return results
 
 	def createUser(self, postData):
 		p_hash = bcrypt.hashpw(postData['password'].encode(), bcrypt.gensalt())
 		user = User.objects.create(
-		    first_name=postData['first_name'], last_name=postData['last_name'], email=postData['email'], password=p_hash)
+		    first_name=postData['first_name'], last_name=postData['last_name'], username = postData['username'], email=postData['email'], password=p_hash, merchant_type = postData['merchant_type'])
 		return user
 
 	def loginVal(self, postData):
 		results = {'status': True, 'errors': [], 'user': None}
-		results['user'] = User.objects.filter(email=postData['email'])
+		results['user'] = User.objects.filter(username=postData['username'])
 		if len(results['user']) < 1:
 			results['status'] = False
-			results['errors'].append('Something Went Wrong.')
+			results['errors'].append('Invalid Login Information')
 		else:
 			hashed = bcrypt.hashpw(postData['password'].encode(
 			    ), results['user'][0].password.encode())
 			if hashed != results['user'][0].password:
 				results['status'] = False
-				results['errors'].append('Something Went Wrong.')
+				results['errors'].append('Invalid Login Information')
 		return results
 
 
 class User(models.Model):
-	first_name = models.CharField(max_length=400)
-	last_name = models.CharField(max_length=400)
-	email = models.CharField(max_length=400)
-	password = models.CharField(max_length=400)
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	username = models.CharField(max_length=20)
+	email = models.CharField(max_length=250)
+	password = models.CharField(max_length=50)
+	merchant_type = models.CharField(max_length=400)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now = True)
 	objects = UserManager()
